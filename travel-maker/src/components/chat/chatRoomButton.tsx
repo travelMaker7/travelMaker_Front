38399820e37f922feda4f13733e00afb7ac3670d
donysony <doffltm9812@gmail.com>
2023-11-27 06:19:21 +0900

    [fix] 머지 후 충돌해결

--- travel-maker/src/components/chat/chatRoomButton.tsx
+++ travel-maker/src/components/chat/chatRoomButton.tsx
@@ -6,46 +6,8 @@ interface ChatRoomControlProps {
   handleCreateAndEnterRoom: () => void;
 }
 
-const ChatRoomButton: React.FC<ChatRoomControlProps> = ({ handleCreateAndEnterRoom }) => {
+const ChatRoomButton: React.FC<ChatRoomControlProps> = ({handleCreateAndEnterRoom }) => {
   
-  const handleCreateAndEnterRoom = () => {
-    console.log('토큰정보 :', localStorage.getItem("access_token"))
-    if(isRoomCreated){
-      console.log("chat room is already created")
-    }
-    // 1:1 채팅방 생성 api 요청 & 입장
-    axios
-      .post(`https://sosak.store/api/v1/chat/room`,{},
-      {
-        headers: {
-          "Authorization" : `Bearer ${localStorage.getItem("access_token")}`,
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-
-        const {chatRoomId, redisRoomId} = response.data.data;
-        setChatRoomId(chatRoomId);
-        setRedisRoomId(redisRoomId);
-        setIsRoomCreated(true);
-        console.log('redis : ',redisRoomId)
-        console.log('chat : ',chatRoomId)
-        const enterRoomUri = `https://sosak.store/api/v1/chat/room/${redisRoomId}?chatRoomId=${chatRoomId}`
-        return axios.get(enterRoomUri, {
-          headers: {
-            "Authorization" : `Bearer ${localStorage.getItem("access_token")}`,
-            "Content-Type": "application/json",
-          },
-        }).then(response => {
-          console.log("Entered the chat room success", JSON.stringify(response.data.data))
-          navigate(`/chat/room/${redisRoomId}?chatRoomId=${chatRoomId}`)
-        });
-      })
-      .catch(error => {
-        console.log("error creating room ", error);
-      });
-  }
-
   return (
     <ChattingButton
     onClick={handleCreateAndEnterRoom}
